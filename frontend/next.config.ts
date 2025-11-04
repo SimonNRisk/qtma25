e607a1414a5c096b4f51021c92c63dd0ab2073b7
Kevin Valencia <kevinangelo.valencia10@gmail.com>
2025-11-03 20:45:31 -0500

    fix(KAN-18): removed source->destination pathing

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,22 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/api/voice/:path*',
-        destination: 'http://localhost:8000/api/voice/:path*',
-      },
-      {
-        source: '/auth/:path*',
-        destination: 'http://localhost:8000/auth/:path*',
-      },
-      {
-        source: '/me',
-        destination: 'http://localhost:8000/me',
-      },
-    ];
-  },
+
 };
 
 export default nextConfig;
